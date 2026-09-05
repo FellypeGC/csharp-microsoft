e28704a80a07ca6f30b602f1729544f80980b66c
FellypeGC <gabrielfellypesteve@gmail.com>
2026-09-05 16:35:24 -0300

    int and string types for variables

--- fundamentals/Program.cs
+++ fundamentals/Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("I'm starting now my jorney to learn C# programming language.");
+﻿using System;
+namespace fundamentals;
+
+
+class Program {
+    static void Main()
+    {
+        int number = 14;
+        string programmingLanguageName = "C#";
+
+        Console.WriteLine("The number is: " + number);
+        Console.WriteLine("This is a simple {0} program", programmingLanguageName);
+    }
+}
